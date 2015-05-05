35cb5b1bbcd1083b37b697f043155e791be11d84
TIANZHI_CHEN <burgesschen1990@gmail.com>
2015-05-05 17:21:32 -0400

    added everything
    added everything

--- search.py
+++ search.py
@@ -64,23 +64,80 @@ def print_summary(rt, num = 10):
 __author__ = 'Michael Yu'
 #get the average rating for each business based on all reviews
 #currently returns dictionary of businesses and the rating information. --can be changed later--
-def avg_user_rating(rt):
-    reviews = {}
+def avg_user_rating(rt,ranking_method):
+    businesses = {}
     #sums up all the ratings
     for rev in rt["hits"]["hits"]:
         bus_id = rev['_source']['business_id']
         rating = rev['_source']['stars']
-        if not reviews.has_key(bus_id):
-            reviews[bus_id] = {'total_ratings': rating, 'num_ratings': 1}
+        highlit = rev['highlight']['text'][0]
+        if not businesses.has_key(bus_id):
+            businesses[bus_id] = {'total_ratings': rating, 'num_ratings': 1, "highlights" : [highlit]}
+
         else:
-            reviews[bus_id]['total_ratings'] += rating
-            reviews[bus_id]['num_ratings'] += 1
+            businesses[bus_id]['total_ratings'] += rating
+            businesses[bus_id]['num_ratings'] += 1
+            businesses[bus_id]["highlights"].append(highlit)
+
     #finds the average of the reviews
-    for bus in reviews:
-        reviews[bus]['avg_rating'] = reviews[bus]['total_ratings']/float(reviews[bus]['num_ratings'])
-        reviews[bus]['logged_avg_rating'] = math.log(reviews[bus]['total_ratings'])*(reviews[bus]['total_ratings'] / float(reviews[bus]['num_ratings']))
-        print reviews[bus]
-    return reviews
+    for bus in businesses:
+        businesses[bus]['avg_rating'] = businesses[bus]['total_ratings']/float(businesses[bus]['num_ratings'])
+        businesses[bus]['logged_avg_rating'] = math.log(businesses[bus]['total_ratings'])*(businesses[bus]['total_ratings'] / float(businesses[bus]['num_ratings']))
+
+    if ranking_method == 'loggedstar':
+        sorted_business = sorted(businesses, key = lambda obje: businesses[obje]["logged_avg_rating"],reverse = True)
+    else:
+        sorted_business = sorted(businesses, key=lambda obje: businesses[obje]["avg_rating"], reverse=True)
+
+    businesses_with_highlights = []
+    for bus in sorted_business:
+        print (bus,businesses[bus]["highlights"])
+        businesses_with_highlights.append((bus,businesses[bus]["highlights"]))
+    return businesses_with_highlights
+
+
+def present_rt_sorted_businesses(sorted_business,number = 10,high_lights_number = 3):
+    f_out = open('present.txt','w')
+    f = open("yelp_businesses_file.json",'r')
+    buses_dict = json.load(f)
+
+    for i in range(number):
+        bus = buses_dict[sorted_business[i][0]]
+        print '==========='+ str(i+1) +'==========='
+        print 'Restaurant: ',bus["name"]
+        print '------------------------------------'
+        print bus["full_address"]
+        print '  -----------  reviews  ------------'
+        for j in range(high_lights_number):
+            if j <= len(sorted_business[1]) - 1:
+                print sorted_business[i][1][j]
+        print ''
+
+        f_out.write('==========='+ str(i+1) +'===========\n')
+        f_out.write('Restaurant: '+ bus["name"].encode('utf-8') +'\n')
+        f_out.write( '------------------------------------\n')
+        f_out.write( bus["full_address"] + '\n')
+        f_out.write( ' -----------  reviews  ------------\n')
+        for j in range(high_lights_number):
+            if j <= len(sorted_business[1]) - 1:
+                f_out.write( sorted_business[i][1][j] + '\n')
+        f_out.write('\n')
+    f.close()
+
+def get_highlights(query_string, bus_id):
+    f_bus = open("yelp_businesses_file.json", 'r')
+    f_review = open("yelp_reviews_file.json",'r')
+    buses_dict = json.load(f_bus)
+    revs_dict = json.load(f_review)
+
+    f_bus.close()
+    f_review.close()
+
+
+
+
+# def get_key_field():
+
 
 
 def reviewFilter_votes(rt, lowerBorder = 0):
@@ -96,9 +153,7 @@ def reviewFilter_votes(rt, lowerBorder = 0):
     print_summary(rtCopy)
 
     return rtCopy
-                 
 
-# TODO: filter out reviews by ratings: ( lucy )
 def reviewFilter_stars(rt, lowerBorder = 0, upperBorder = 5):
     rtCopy = copy.deepcopy(rt)
     newHits = []
@@ -121,9 +176,18 @@ def reviewFilter_stars(rt, lowerBorder = 0, upperBorder = 5):
             # TODO: INTERFACE
 
 
-def search_combined(query, star_cut_off = 0, usfellness_cut_off = 0):
-    rt = q_mw('good',True, star_cut_off, usfellness_cut_off)
-    avg_user_rating(rt)
+def search_combined(query, star_cut_off = 0,
+                    usfellness_cut_off = 0,
+                    sorted_number = 10,
+                    highlights = 3, ranking_method = 'star'):
+    # methods : star, loggedstar,
+
+    rt = q_mw('good',False, star_cut_off, usfellness_cut_off)
+    if ranking_method == 'star':
+        sorted_businesses = avg_user_rating(rt)
+    elif ranking_method == 'loggedstar':
+
+    present_rt_sorted_businesses(sorted_businesses,sorted_number,highlights)
 
 if __name__ == '__main__':
     rt = q_mw('paella')
@@ -134,5 +198,10 @@ if __name__ == '__main__':
     search_combined("good", star_cut_off=0, usfellness_cut_off=0)
 
 
+
+    print
+    #reviewFilter_votes(rt, 100)
+    print
+    #reviewFilter_stars(rt, 2, 3)
 
 
